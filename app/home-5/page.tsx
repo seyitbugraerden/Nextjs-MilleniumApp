b2c0adacb20944a8a7a14bfd464b0795064a1f33
seyitbugraerden <seyitbugraerden@gmail.com>
2024-12-18 23:53:19 +0300

    first commit

---
+++ app/home-5/page.tsx
@@ -0,0 +1,12 @@
+import Cta8 from "@/components/sections/Cta8";
+
+import Layout from "@/components/layout/Layout";
+export default function Home5() {
+  return (
+    <>
+      <Layout headerStyle={5} footerStyle={4}>
+        <Cta8 />
+      </Layout>
+    </>
+  );
+}
