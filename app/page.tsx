faa039506feb81f7a42b938a6af5580d5616d8b8
seyitbugraerden <seyitbugraerden@gmail.com>
2025-01-01 01:39:07 +0300

    Main section

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,20 @@
 import Cta8 from "@/components/sections/Cta8";
-import Layout from "@/components/layout/Layout";
-import Blog1 from "@/components/sections/Blog1";
-import Hero1 from "@/components/sections/Hero1";
 import Static3 from "@/components/sections/Static3";
 import Features1 from "@/components/sections/Features1";
 import Services1 from "@/components/sections/Services1";
-import LogoCloud1 from "@/components/sections/LogoCloud1";
 import Testimonial1 from "@/components/sections/Testimonial1";
-import Hero3 from "@/components/sections/Hero3";
+import Hero2 from "@/components/sections/Hero2";
+import Features2 from "@/components/sections/Features2";
+import Features3 from "@/components/sections/Features3";
+import Features4 from "@/components/sections/Features4";
 
 export default function Home() {
   return (
     <>
-      <Hero3 />
-      <Features1 />
+      <Hero2 />
       <Cta8 />
-      <Services1 />
+      <Features4 />
       <Static3 />
-      <Testimonial1 />
     </>
   );
 }
