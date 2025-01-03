152b052088846705a31a42516579c3fafcc1b523
seyitbugraerden <seyitbugraerden@gmail.com>
2025-01-03 22:47:00 +0300

    layout update

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="manifest" href="/manifest" />
 
         <link
           rel="apple-touch-icon"
@@ -100,7 +99,6 @@ export default function RootLayout({
           sizes="16x16"
           href="/favicon-16x16.png"
         />
-        <link rel="manifest" href="/manifest.json" />
         <meta name="msapplication-TileColor" content="#ffffff" />
         <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
         <meta name="theme-color" content="#ffffff" />
