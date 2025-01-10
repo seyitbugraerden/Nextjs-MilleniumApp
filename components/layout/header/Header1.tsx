aa38f1383df05a16cade56159c868a506215d2e2
seyitbugraerden <seyitbugraerden@gmail.com>
2025-01-10 23:11:12 +0300

    header name fix

--- components/layout/header/Header1.tsx
+++ components/layout/header/Header1.tsx
@@ -25,7 +25,7 @@ export default function Header1({
               className="navbar-brand d-flex main-logo align-items-center"
               href="/"
             >
-              <img src="/logo.svg" alt="infinia" width={200} />
+              <img src="/logo.svg" alt="Millenium" width={200} />
             </Link>
             <Menu />
             <div className="d-flex align-items-center pe-5 pe-lg-0 me-5 me-lg-0">
