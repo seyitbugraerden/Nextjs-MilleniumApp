f89a46e68b26a796d6ae08de4539fc83f55dff56
seyitbugraerden <seyitbugraerden@gmail.com>
2025-05-27 00:01:55 +0300

    iletisim title

--- app/iletisim/page.tsx
+++ app/iletisim/page.tsx
@@ -15,7 +15,7 @@ const page = () => {
       <section className="section-page-header py-10 fix position-relative">
         <div className="container position-relative z-1">
           <div className="text-start">
-            <h1 style={{ fontSize: "38px !important" }}>İletişim</h1>
+            <h1 style={{ fontSize: "38px !important" }}>Bizimle İletişime Geçin!</h1>
             <div className="d-flex">
               <Link href="/">
                 <p className="mb-0 text-900">Ana Sayfa</p>
